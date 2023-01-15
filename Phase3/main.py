3828f2f852903c979227914950b5f24079ff3783
stephenm7777 <122754065+stephenm7777@users.noreply.github.com>
2023-01-15 14:00:38 -0800

    Add files via upload

---
+++ Phase3/main.py
@@ -0,0 +1,33 @@
+from menu_helper import *
+from functions import *
+from menu_display import menu
+from populate import populate
+
+if __name__ == '__main__':
+    # Menu display
+
+    while True:
+        choice = menu()
+
+        if choice == 0:
+            populate()
+        elif choice == 1:
+            create_key()
+        elif choice == 2:
+            request_access()
+        elif choice == 3:
+            unauthorized_issue_key()
+        elif choice == 4:
+            losing_key()
+        elif choice == 5:
+            accessible_rooms()
+        elif choice == 6:
+            delete_key()
+        elif choice == 7:
+            delete_employee()
+        elif choice == 8:
+            add_door()
+        elif choice == 9:
+            update_request()
+        elif choice == 10:
+            report_employees()
